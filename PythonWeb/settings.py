17f98989a965887ff6928a774f5dbbe8c56afbcc
PhuongLH5 <helloitpdu@gmail.com>
2018-07-23 20:59:15 +0700

    PhuongLH5: commit project django

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [u'18.211.128.209', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PythonWeb/urls.py
+++ PythonWeb/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
 
-urlpatterns = [ 
+urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('home.ulrs')), 
+    url(r'^', include('home.ulrs')),
 ]
